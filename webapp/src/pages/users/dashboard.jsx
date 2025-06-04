a0248b978c1e7b1f10f50845133cc56bfe30df9d
mire-web <amahe8664@gmail.com>
2025-06-04 14:38:05 +0100

    still-working-on-dashboard

--- webapp/src/pages/users/dashboard.jsx
+++ webapp/src/pages/users/dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RiHome4Line, RiMedicineBottleLine, RiCalendarLine, RiBankCardLine, RiSettings2Line, RiCustomerServiceLine, RiArrowRightSLine } from '@remixicon/react'
+import { RiHome4Line, RiMedicineBottleLine, RiCalendarLine, RiBankCardLine, RiSettings2Line, RiCustomerServiceLine, RiArrowRightSLine, RiVerifiedBadgeFill } from '@remixicon/react'
 import linepath from '../../assets/icons/line-path.png'
 import logo from '../../assets/images/logo-transparent.png'
 
@@ -7,8 +7,8 @@ import logo from '../../assets/images/logo-transparent.png'
 function Dashboard() {
 	return (
 		<div className='grid grid-cols-[auto_1fr] gap-4'>
-			<div className='bg-oasis-indigo w-70 h-[100vh] overflow-hidden'>
-				<img src={linepath} alt="" className='fixed top-[-100px] left-[-20px]'/>
+			<div className='bg-oasis-indigo w-80 h-[100vh] flex flex-col overflow-hidden'>
+				<img src={linepath} alt="" className='fixed top-[-100px] left-[-20px]' />
 				<div className='m-5 mt-10 pb-10 flex border-b-1 border-gray-500'>
 					<img src={logo} alt="" />
 					<div className='ml-3'>
@@ -16,46 +16,55 @@ function Dashboard() {
 						<p className='text-oasis-white'>Your Health Reminder</p>
 					</div>
 				</div>
-				<div className='relative'>
-				<div className='w-60 flex ml-5 h-[100%] items-center mt-5 p-3 rounded-2xl active'>
-					<RiHome4Line size={18} className='fill-oasis-yellow icon' />
-					<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Dashboard</h3>
-				</div>
-				</div>
-				<div className='relative'>
-				<div className='w-[100%] flex items-center mt-5 pl-10'>
-					<RiMedicineBottleLine size={18} className='fill-oasis-yellow icon' />
-					<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Medications</h3>
-				</div>
-				</div>
-				<div className='relative'>
-				<div className='w-[100%] flex items-center mt-5 pl-10'>
-					<RiCalendarLine size={18} className='fill-oasis-yellow icon' />
-					<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Schedule</h3>
-				</div>
-				</div>
-				<div className='relative'>
-				<div className='w-[100%] flex items-center mt-5 pl-10'>
-					<RiBankCardLine size={18} className='fill-oasis-yellow icon' />
-					<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Subscriptions</h3>
-				</div>
-				</div>
-				<footer>
-					<div>
-						<div>
-							<RiSettings2Line size={18} className='fill-oasis-yellow'/>
+				<div className='h-[100%]'>
+					<div className='relative flex items-center h-[40px] mt-5'>
+						<div className='w-60 flex ml-5 h-[100%] items-center p-3 rounded-2xl active'>
+							<RiHome4Line size={18} className='fill-oasis-yellow icon' />
+							<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Dashboard</h3>
 						</div>
-						<div>
+					</div>
+					<div className='relative flex items-center h-[40px] mt-5'>
+						<div className='w-60 flex ml-5 h-[100%] items-center p-3 rounded-2xl'>
+							<RiMedicineBottleLine size={18} className='fill-oasis-yellow icon' />
+							<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Medications</h3>
+						</div>
+					</div>
+					<div className='relative flex items-center h-[40px] mt-5'>
+						<div className='w-60 flex ml-5 h-[100%] items-center p-3 rounded-2xl'>
+							<RiCalendarLine size={18} className='fill-oasis-yellow icon' />
+							<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Schedule</h3>
+						</div>
+					</div>
+					<div className='relative flex items-center h-[40px] mt-5'>
+						<div className='w-60 flex ml-5 h-[100%] items-center p-3 rounded-2xl'>
+							<RiBankCardLine size={18} className='fill-oasis-yellow icon' />
+							<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Subscriptions</h3>
+						</div>
+					</div>
+				</div>
+				<footer className=''>
+					<div>
+						<div className='w-60 flex ml-5 h-[100%] items-center p-3 rounded-2xl'>
+							<RiSettings2Line size={18} className='fill-oasis-yellow' />
+							<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Settings</h3>
+						</div>
+						<div className='w-60 flex ml-5 h-[100%] items-center p-3 rounded-2xl'>
 							<RiCustomerServiceLine size={18} className='fill-oasis-yellow' />
+							<h3 className='ml-3 text-oasis-yellow text-lg/[20px] tracking-[0.25%'>Support</h3>
 						</div>
 					</div>
 					<div>
-						<div></div>
 						<div>
-							<div></div>
-							<div></div>
+							{/* <img src={avatar} alt="" /> */}
 						</div>
-						<RiArrowRightSLine size={18} className='fill-oasis-yellow'/>
+						<div>
+							<div>
+								Sophia Williams
+								<RiVerifiedBadgeFill />
+							</div>
+							<div>sophiawilliams@gmail.com</div>
+						</div>
+						<RiArrowRightSLine size={18} className='fill-oasis-yellow' />
 					</div>
 				</footer>
 			</div>
